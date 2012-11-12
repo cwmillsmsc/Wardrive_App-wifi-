27ac948c04a0d3a06566dc7bb649dd52ba260b36
cameron mills <spacemans@hotmail.com>
2012-11-12 10:33:13 -0700

    Init Files

---
+++ gen/com/wardrive/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.wardrive;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/wardrive/MyPhoneGapActivity.java
@@ -0,0 +1,13 @@
+package com.wardrive;
+
+import org.apache.cordova.DroidGap; 
+import android.os.Bundle;
+
+
+public class MyPhoneGapActivity extends DroidGap {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+}
